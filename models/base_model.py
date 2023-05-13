6582b91735d77f05d5b79b56269ac4a75b5a6c53
saria-mohi <saria.m.mohammed@gmail.com>
2023-05-13 21:43:57 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
+"""BaseModel Class."""
 import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """Super class content attributes and methods for other classes"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+        """Initial value of instance attributes
 
         Args:
             *args (any): Unused.
@@ -29,16 +29,14 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """updates the public instance attribute updated_at with the current
+        datetime"""
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
+        """returns a dictionary containing all keys/values of __dict__ of the
+        instance"""
         rdict = self.__dict__.copy()
         rdict["created_at"] = self.created_at.isoformat()
         rdict["updated_at"] = self.updated_at.isoformat()
